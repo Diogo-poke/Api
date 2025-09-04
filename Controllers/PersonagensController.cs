df6482eac60810052cf50c4a5bf97575fd504399
Diogo-poke <diogopda2007@gmail.com>
2025-09-03 22:34:07 -0300

    Fazer Exercicio

--- Controllers/PersonagensController.cs
+++ Controllers/PersonagensController.cs
@@ -53,5 +53,21 @@ namespace RpgApi.Controllers
             return Ok(personagens);
           }
 
+         [HttpPut]   
+
+         public IActionResult  UpdatePerosnagem(Personagem p)
+         {
+            Personagem personagemAlterado = personagens.Find(pers => pers.Id == p.Id);
+            personagemAlterado.Nome = p.Nome;
+            personagemAlterado.PontosVida = p.PontosVida;
+            personagemAlterado.Forca = p.Forca;
+            personagemAlterado.Defesa = p.Defesa;
+            personagemAlterado.Inteligencia = p.Inteligencia;
+            personagemAlterado.Classe = p.Classe;
+
+            return Ok(personagens);
+         }
+
+
     }
 }
