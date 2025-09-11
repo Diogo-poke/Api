9c0bb8be3134f3fd239087f0eb62395c144891fc
Diogo-poke <diogopda2007@gmail.com>
2025-09-10 22:29:30 -0300

    Tabela_PERSONAGENS

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using RpgApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+   options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoLocal"));
+});
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -6,6 +14,7 @@ builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
